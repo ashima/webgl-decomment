8cc66e5106a6549ac431e3b6ce868d5544879957
ijm <ijm@ashimaarts.com>
2011-01-14 16:38:34 -0800

    updated the javascript version

--- decomment.js
+++ decomment.js
@@ -2,27 +2,23 @@ function decomment(x)
   {
   var i = 0, s = 0, o = "";
   while (i < x.length)
-    switch (s)
+    { 
+    var c = x[i++], d = x[i];
+    if ( c == "\n" )
+      o += c;
+    else if ( s==2 && c=="*" && d== '/' )
+      s = 0, i++;
+    else if ( s==1 && d == '\n' )
+      s = 0;
+    else if ( s==0 )
       {
-      case 2:
-        if ( x[i] == "\n" )
-          o += x[i];
-        if ( x[i++] == "*" && x[i] == "/" )
-          s=0, i++;
-        break;
-      case 1:
-        if ( x[i] == "\n" )
-          s = 0;
-        else
-          i++;
-        break;
-      default:
-        var c = x[i++];
-        if ( c == "/" && (s={"/":1,"*":2}[x[i]]) )
-          c = ' ', i++;
-        o += c;
+      if ( c == "/" && (s={"/":1,"*":2}[d]|0))
+        c = ' ', i++;
+      o += c;
       }
+    }
   return o; // @~ George Santayana
   }
 
 print(decomment(arguments[0]));
+
